78118a3e441234a36036823783fb5d961c580fa1
Andrey <a-shirokov@yandex.ru>
2022-04-16 17:45:33 +0300

    Delete timer.sh

--- timer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-timer=`zenity --entry --title="ТАЙМЕР" --text="сингал через.... минут, например" --entry-text="10"`; echo "mpv /home/user/Музыка/Звуки_уведомлений/alarm_clock_труба.mp3" | at now + $timer min
