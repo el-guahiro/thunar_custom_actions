1b669caf351c9a2625f49aef496ae9ff61468ff7
Andrey <a-shirokov@yandex.ru>
2025-01-05 01:08:27 +0300

    Delete light-picture.sh
    A full update of all scripts is planned

--- light-picture.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#from el guahiro, USSR
-IFS=$'\#'
-for i in "$@" ; do convert  "$i" -morphology Convolve DoG:15,100,0 -modulate 100,150,100 -negate -normalize -blur 0x0.7 -channel RBG -level 60%,91%,0.1  "$i".2  ; mv -v "$i".2 "$i" ; done |zenity --progress --width=400 --height=100 --title="Делаем читабельно" --text "работаю." --auto-close --pulsate;notify-send 'Теперь годится для печати!'
- exit
-                 fi)
