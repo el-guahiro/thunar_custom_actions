3ba7412c302e7fe34b870905de19d794470cf2ed
Andrey <a-shirokov@yandex.ru>
2025-01-05 01:02:28 +0300

    Delete compress-pdf-standart.sh
    A full update of all scripts is planned

--- compress-pdf-standart.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-IFS=$'\#'
-for i in "$@" ; do ps2pdf -dUseFlateCompression=true -dOptimize=true -dProcessColorModel=/DeviceRGB -dDownsampleGrayImages=true -dAutoFilterColorImages=false -dDownsampleColorImages=true -dDownsampleMonoImages=true -dGrayImageDownsampleType=/Bicubic "$i" "$i"2 ; mv -v "$i"2 "$i";
-done
