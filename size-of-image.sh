9a0c404beb27fce5cdb3d933a2748d0d49a4eb1f
Andrey <a-shirokov@yandex.ru>
2025-01-05 01:09:27 +0300

    Delete size-of-image.sh
    A full update of all scripts is planned

--- size-of-image.sh
+++
@@ -1,4 +0,0 @@
-Изменить разрешение изображений
-AAA=`zenity --entry --title="ImageMagick-resize" --text="Введите ширину в пикселях" --entry-text="800"`;if [ $? = 0 ];then for file in "$@";do mogrify -resize $AAA "$file"; done
-      exit
-                 fi)
