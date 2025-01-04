0aa817a1fb86261d3dccc42a92793096ecda69a9
Andrey <a-shirokov@yandex.ru>
2025-01-05 01:06:44 +0300

    Delete fast-resize-pdf.sh
    A full update of all scripts is planned

--- fast-resize-pdf.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-IFS=$'\#'
-for i in "$@" ; do ps2pdf -dUseFlateCompression=true -dOptimize=true -dProcessColorModel=/DeviceRGB  -r72 -dDownsampleGrayImages=true -dAutoFilterColorImages=false -dDownsampleColorImages=true -dDownsampleMonoImages=true -dGrayImageDownsampleType=/Bicubic "$i" "$i".2 ; mv -v "$i".2 "$i" ; done | zenity --progress --width=400 --height=100 --title="Сжимаем PDF" --text "работаю" --auto-close --pulsate;exiftool -r -overwrite_original -P -all= %N;notify-send 'Сжали PDF в три раза!'
- exit
-                 fi)
